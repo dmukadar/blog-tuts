dd89d24a86c83475786469d6cb34ef4557c68edc
Mochamad Karsa SYaifudin Jupri <karsaaif87@gmail.com>
2015-10-20 11:11:28 +0700

    add folder vendor

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -15,12 +15,25 @@ use yii\web\AssetBundle;
  */
 class AppAsset extends AssetBundle
 {
-    public $basePath = '@webroot';
-    public $baseUrl = '@web';
+//     public $basePath = '@webroot';
+//     public $baseUrl = '@web';
+    public $sourcePath = '@bower/developer/';
     public $css = [
-        'css/site.css',
+//         'css/site.css',
+    	'assets/plugins/bootstrap/css/bootstrap.min.css',
+    	'assets/plugins/font-awesome/css/font-awesome.css',
+    	'assets/plugins/github-activity/dist/github-activity-0.1.1.min.css',
+    	'assets/plugins/github-activity/dist/octicons/octicons.min.css',
+    	'assets/css/styles.css',
     ];
     public $js = [
+    	'assets/plugins/jquery-1.11.2.min.js',
+    	'assets/plugins/jquery-migrate-1.2.1.min.js',
+    	'assets/plugins/bootstrap/js/bootstrap.min.js',
+    	'assets/plugins/jquery-rss/dist/jquery.rss.min.js',
+    	'assets/plugins/github-activity/dist/mustache/mustache.min.js',
+    	'assets/plugins/github-activity/dist/github-activity-0.1.1.min.js',
+    	'assets/js/main.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
