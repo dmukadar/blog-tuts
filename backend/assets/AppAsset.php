e2a79159a3af68d69911cf564df7ae065289afbc
heriwahyudianto <heriwahyudianto@yahoo.co.id>
2015-10-21 12:58:53 +0700

    layout adminLTE

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -21,13 +21,23 @@ class AppAsset extends AssetBundle
         'bootstrap/css/bootstrap.css',
         'css/font-awesome.min.css',
         'css/ionicons.min.css',
+        'plugins/jvectormap/jquery-jvectormap-1.2.2.css',
         'dist/css/AdminLTE.css',
         'plugins/iCheck/square/blue.css',
+        'dist/css/skins/_all-skins.min.css',
     ];
     public $js = [
         'plugins/jQuery/jQuery-2.1.4.min.js',
         'bootstrap/js/bootstrap.min.js',
         'plugins/iCheck/icheck.min.js',
+        'plugins/fastclick/fastclick.min.js',
+        'dist/js/app.min.js',
+        'plugins/sparkline/jquery.sparkline.min.js',
+        'plugins/jvectormap/jquery-jvectormap-1.2.2.min.js',
+        'plugins/jvectormap/jquery-jvectormap-world-mill-en.js',
+        'plugins/slimScroll/jquery.slimscroll.min.js',
+        'plugins/chartjs/Chart.min.js',
+        'dist/js/pages/dashboard2.js',
         'js/ready.js',
     ];
     public $depends = [
